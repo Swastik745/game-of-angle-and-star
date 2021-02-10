eef42fc2847308658f4a8808a58899f634a56201
Swastik745 <77013130+Swastik745@users.noreply.github.com>
2021-02-10 14:08:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,67 +1,72 @@
-var ground,groundImage;
-var cat,catImage1,catIm2,catImage3;
-var mouse,mouseImage1,mouseIm2,mouseImage3;
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
 
-function preload() {
-   groundImage = loadImage("garden.png");
-   catImage1 = loadImage("cat1.png");
-   mouseImage1 = loadImage("mouse1.png");
-   catIm2 = loadAnimation("cat2.png","cat3.png");
-   catImage3 = loadAnimation("cat4.png");
-   mouseIm2 = loadAnimation("mouse2.png","mouse3.png");
-   mouseImage3 = loadAnimation("mouse4.png");
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("star.png");
+	fairyImg = loadAnimation("fairyImage1.png","fairyImage2.png");
+	bgImg = loadImage("starNight.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
+
 }
 
 function setup() {
-  createCanvas(875,625);
-   
-  ground = createSprite(438,310);
-  ground.addImage(groundImage);
-  ground.scale = 0.9;
+	createCanvas(800, 750);
 
-  cat = createSprite(800,480);
-  cat.addImage(catImage1);
-  cat.scale = 0.1;
-  
+	
 
-  mouse = createSprite(150,480);
-  mouse.addImage(mouseImage1);
-  mouse.scale = 0.10;
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
 
 }
+
 
 function draw() {
-   background("blue");
+  background(bgImg);
 
-   
+  if(starBody.position.y > 270 ) {
+	  star.x = starBody.position.x
+	  star.y = starBody.position.y
+	  star.velocityY = 0;
+  }
 
-   if(cat.x - mouse.x < (cat.width - mouse.width)/2) {
-       cat.velocityX = 0;
-       cat.addAnimation("catlastImage",catImage3);
-       cat.changeAnimation("catlastImage",);
-       cat.x = 200;
-       cat.y = 480;
-
-       mouse.addAnimation("mouseLastImage",mouseImage3);
-       mouse.changeAnimation("mouseLastImage");
-   }
-
-   keyPressed ();
+  keyPressed();
   drawSprites();
+
 }
 
-function keyPressed () {
-
-  if(keyDown(LEFT_ARROW)) {
-     cat.velocityX = -5;
-     cat.addAnimation("catRunning",catIm2);
-     cat.changeAnimation("catRunning");
-
-               
-     mouse.addAnimation("mouseTalking",mouseIm2)
-     mouse.changeAnimation("mouseTalking");
-  }
-}   
-
-
-
+function keyPressed() {
+	//write code here
+	if(keyDown(LEFT_ARROW)){
+		fairy.x=fairy.x-3;
+		fairyVoice.play();
+	}
+	
+	if(keyDown(RIGHT_ARROW)){
+		fairy.x=fairy.x+3;
+		fairyVoice.play();
+	}
+	
+	if(keyDown(DOWN_ARROW)){
+		star.velocityY = 4;
+    }
+}
